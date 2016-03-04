4cdb2b954e6fe31bc0ba948eddffa55fd6a0af7d
Nelson Kuang <410374308@qq.com>
2016-03-04 15:43:19 +0800

    Update ProvinceSelect.jsx

--- assets/js/ProvinceSelect.jsx
+++ assets/js/ProvinceSelect.jsx
@@ -1,1 +1,38 @@
+var React = require('react');
+var DefaultOption = require('./DefaultOption.jsx');
+var ProvinceOption = require('./ProvinceOption.jsx');
 
+var ProvinceSelect = React.createClass({
+  handleChange: function() {
+    this.props.onUserSelect(this.refs.provSelect.value);
+  },  
+  render: function() {
+    var options=[];
+    options.push(<DefaultOption />);
+    this.props.provinces.forEach(function(province){
+      options.push(<ProvinceOption province={province} key={province.value} />);
+    }.bind(this));
+    return (
+        <li className="clearfix">
+            <div className="fl a">{"省份"}</div>
+            <div className="fl b">
+                <div className="new-select-wp" id="prov">
+                    <select 
+                      className="select-31" 
+                      id="prov_select" 
+                      name="prov" 
+                      value={this.props.initSelectedValue}
+                      ref="provSelect"
+                      onChange={this.handleChange}
+                    >
+                      {options}     
+                    </select>
+                </div>
+            </div>
+        </li>
+      );
+  }
+});
+
+
+module.exports = ProvinceSelect;
