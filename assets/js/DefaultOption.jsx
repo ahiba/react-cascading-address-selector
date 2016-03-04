056129b963cea80c3f91ab8d0a4ae6e6925bcf7f
Nelson Kuang <410374308@qq.com>
2016-03-04 15:41:47 +0800

    Update DefaultOption.jsx

--- assets/js/DefaultOption.jsx
+++ assets/js/DefaultOption.jsx
@@ -1,1 +1,16 @@
 
+var React = require('react');
+
+var DefaultOption = React.createClass({
+  render: function() {
+    return (
+      <option 
+         value = ''
+      >
+      {'请选择...'}
+      </option>
+      );
+  }
+});
+
+module.exports = DefaultOption;
