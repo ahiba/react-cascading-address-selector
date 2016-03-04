10bd1018f0a421af0cfc5ecb872a45b7a8d24467
Nelson Kuang <410374308@qq.com>
2016-03-04 15:42:57 +0800

    Update ProvinceOption.jsx

--- assets/js/ProvinceOption.jsx
+++ assets/js/ProvinceOption.jsx
@@ -1,1 +1,16 @@
+var React = require('react');
 
+var ProvinceOption = React.createClass({
+  render: function() {
+    return (
+      <option 
+         value = {this.props.province.value}
+      >
+      {this.props.province.name}
+      </option>
+      );
+  }
+});
+
+
+module.exports = ProvinceOption;
