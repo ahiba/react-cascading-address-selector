a1a000ed1eb0f3b7798af3812081137740dd46c1
Nelson Kuang <410374308@qq.com>
2016-03-04 15:41:29 +0800

    Update CitySelect.jsx

--- assets/js/CitySelect.jsx
+++ assets/js/CitySelect.jsx
@@ -1,1 +1,37 @@
+var React = require('react');
+var DefaultOption = require('./DefaultOption.jsx');
+var CityOption = require('./CityOption.jsx');
 
+var CitySelect = React.createClass({
+  handleChange: function() {
+    this.props.onUserSelect(this.refs.citySelect.value);
+  },   
+  render: function() {
+    var options=[];
+    options.push(<DefaultOption />);
+    this.props.cities.forEach(function(city){
+      options.push(<CityOption city={city} key={city.value} />);
+    }.bind(this));
+    return (
+        <li className="clearfix">
+            <div className="fl a">{"市"}</div>
+            <div className="fl b">
+                <div className="new-select-wp" id="city">
+                    <select
+                      className="select-31" 
+                      id="city_select" 
+                      name="city" 
+                      value={this.props.initSelectedValue}
+                      ref="citySelect"                      
+                      onChange={this.handleChange}
+                    >                   
+                      {options}     
+                    </select>
+                </div>
+            </div>
+        </li>
+      );
+  }
+});
+
+module.exports = CitySelect;
