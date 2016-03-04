7552d1b1d5c1c56ebc9b4c4978f7f0bd901c28d7
Nelson Kuang <410374308@qq.com>
2016-03-04 15:42:05 +0800

    Update DistrictOption.jsx

--- assets/js/DistrictOption.jsx
+++ assets/js/DistrictOption.jsx
@@ -1,1 +1,16 @@
+var React = require('react');
 
+var DistrictOption = React.createClass({
+  render: function() {
+    return (
+      <option 
+         value = {this.props.district.value}
+      >
+      {this.props.district.name}
+      </option>
+      );
+  }
+});
+
+
+module.exports = DistrictOption;
